eadd6619274d0d316902d1c2e44d164e719ed640
ferdek <bystrzak14@gmail.com>
2020-03-19 14:17:51 +0100

    Move project files to a package

--- revolut_to_homebank/revolut_to_homebank.py
+++ revolut_to_homebank/revolut_to_homebank.py
@@ -1,11 +1,11 @@
+import argparse
 import csv
-import sys
 from dataclasses import asdict
 
-from models import RevolutEntry, HomeBankEntry
+from revolut_to_homebank.models import RevolutEntry, HomeBankEntry
 
 
-def main(input_file, delimiter=';'):
+def convert(input_file, output_file="output.csv", delimiter=';'):
     with open(input_file) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=delimiter, skipinitialspace=True)
         line_count = 0
@@ -23,7 +23,7 @@ def main(input_file, delimiter=';'):
                 line_count += 1
         print(f'Processed {line_count} lines.')
 
-    with open("output.csv", 'w') as output_file:
+    with open(output_file, 'w') as output_file:
         writer = csv.writer(output_file, delimiter=delimiter)
         writer.writerows([asdict(hb_entry).values() for hb_entry in hb_entries])
 
@@ -34,5 +34,15 @@ def convert_to_homebank(row):
     return HomeBankEntry.from_revolutentry(revolut_entry)
 
 
+def main():
+    parser = argparse.ArgumentParser(description='revolut_to_homebank.py')
+    parser.add_argument('source', metavar='filepath', type=str, help='file tp be converted')
+    parser.add_argument('-o', '--output', metavar='filename', default='output.csv', help='filename output name')
+    parser.add_argument('-d', '--delimiter', metavar='delimiter', default=';', help='delimiter')
+
+    args = parser.parse_args()
+    convert(args.source, output_file=args.output, delimiter=args.delimiter)
+
+
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main()
